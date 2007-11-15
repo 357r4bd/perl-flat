df1c35b1b48bae701154aa30852e8ee6a860fc66
B. Estrade <estrabd@gmail.com>
2007-11-15 01:22:01 +0000

    updated tests, looks like the fix didn't break anything

--- tinderbox/string-gen.pl
+++ tinderbox/string-gen.pl
@@ -1,7 +1,7 @@
 use Test::More 'no_plan';
 
 use strict;
-use lib "../lib";
+use lib '../lib';
 
 use FLAT;
 use FLAT::DFA;
@@ -10,17 +10,36 @@ use FLAT::PFA;
 use FLAT::Regex::WithExtraOps;
 use FLAT::Regex::Util;
 
+print "Be prepared to wait a really long time :)...\n";
+
+# Evil in the sense that these caused a bug in FLAT::NFA::as_dfa to rear its ugly head ...
+my @EVIL=qw/a* a*+b a*+b* a*&b a*&b*/;
+foreach my $evil (@EVIL) {
+  my $PRE = FLAT::Regex::WithExtraOps->new($evil);
+  my $DFA = $PRE->as_pfa->as_nfa->as_dfa->as_min_dfa->trim_sinks;
+  # default depth
+  my $next = $DFA->new_acyclic_string_generator();
+  while (my $string = $next->()) {
+    ok( $DFA->is_valid_string($string) );
+  }
+  # much deeper
+  $next = $DFA->new_deepdft_string_generator(2000);
+  while (my $string = $next->()) {
+    ok( $DFA->is_valid_string($string) );
+  }
+}
+
 for (1..1000) {
   my $PRE = FLAT::Regex::Util::random_pre(8);
   my $DFA = $PRE->as_pfa->as_nfa->as_dfa->as_min_dfa->trim_sinks;
+  # default depth
   my $next = $DFA->new_acyclic_string_generator();
   while (my $string = $next->()) {
-    print "$string ";
     ok( $DFA->is_valid_string($string) );
   }
-  $next = $DFA->new_deepdft_string_generator(5);
+  # a little deeper
+  $next = $DFA->new_deepdft_string_generator(200);
   while (my $string = $next->()) {
-    print "$string ";
     ok( $DFA->is_valid_string($string) );
   }
 }
