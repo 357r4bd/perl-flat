b5d5edf02b4fd6c1c5d37fa0d108ad3b2a954a16
B. Estrade <estrabd@gmail.com>
2007-11-16 17:07:57 +0000

    made a script that autopackages FLAT for cpan

--- util/make-distro.sh
+++ util/make-distro.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
+OK=0
+
 mkdir FLAT-$$                 &&\
 cp -r ../lib FLAT-$$          &&\
 cp -r ../t FLAT-$$            &&\
 cp -r ../MANIFEST FLAT-$$     &&\
 cp -r ../Makefile.PL FLAT-$$  &&\
-cp -r ../bin FLAT-$$
+cp -r ../bin FLAT-$$          &&\
+cd FLAT-$$                    &&\
+perl Makefile.PL              &&\
+make test                     &&\
+make clean                    &&\
+cd ..                         &&\
+tar -v --exclude=.svn --exclude=*~  --create --file=- FLAT-$$ | gzip -c > FLAT-$$.tar.gz
 
-find FLAT-$$/ -name ".svn" -exec rm -rf {} \;
-tar cvf - FLAT-$$ | gzip -c > FLAT-$$.tar.gz
+echo things look ok, let\'s check    &&\
+rm -rf FLAT-$$                       &&\
+gunzip -c FLAT-$$.tar.gz | tar xvf - &&\
+cd FLAT-$$                           &&\
+perl Makefile.PL                     &&\
+make test && cd .. && rm -rf FLAT-$$ && OK=1
+
+if [ $OK -eq 1 ]; then
+  echo things look ok
+else
+  echo things broke
+fi
