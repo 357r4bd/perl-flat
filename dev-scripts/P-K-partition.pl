183ea497be96fa4434e700b64a2cbe69ee96b087
B. Estrade <estrabd@gmail.com>
2008-10-31 21:09:09 +0000

    saving 1/2 done stub

--- dev-scripts/P-K-partition.pl
+++ dev-scripts/P-K-partition.pl
@@ -10,7 +10,11 @@ my $dfa = FLAT::Regex::WithExtraOps->new($ARGV[0])->as_pfa->as_nfa->as_dfa->as_m
 # initial DFT, detecting strong components *and*
 # the nodes which are accessible by them
 sub pass1 {
+  my $startNode = shift;
+  my $nodelist  = shift;
+  foreach my $adjacent (keys(%{$nodelist})) {
 
+  }
 }
 
 # secondary DFT over forest, of which each k-group
@@ -20,16 +24,27 @@ sub pass2 {
 
 }
 
-sub pk_partition {
-  my @string  = (); 
-  my $lastDFLabel = 0; 
-  my %nodelist = $dfa->as_node_list(); 
-  my %dflabel = (); 
-  my %backtracked = (); # tracks nodes from which we've backtracked for cross edge detection
-  my %taintpoints = (); # tracks taint points
-  my %kgroups = (); # strong components + nodes reachable via them
-  my %crossedges = (); # tracks cross edges to check 
+# builds a new directed graph taking all nodes in
+# each k-group to be their own node
+sub consolidate {
+
 }
 
+sub pk_partition {
+  my $dfa = shift;
+  my $lastDFLabel = 0; 
+  my %nodelist    = $dfa->as_node_list(); 
+  my %dflabel     = (); # tracks dflabel 
+  my %backtracked = (); # tracks nodes from which we've backtracked for cross edge detection
+  my %taintpoints = (); # tracks taint points
+  my %kgroups     = (); # strong components + nodes reachable via them
+  my %crossedges  = (); # tracks cross edges to check 
+  pass1($dfa->get_starting(),\%nodelist);
+  #...
+  %nodelist = consolidate(\%nodelist,\%kgroups,\%crossedges);
+  pass2()
+}
+
+my (@P,@K) = pk_partition($dfa);
 
 1;
