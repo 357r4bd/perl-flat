46211bb9baa769e9b48a9b100137a90fc815385d
B. Estrade <estrabd@gmail.com>
2008-11-04 14:33:04 +0000

    setting up test case, need to implement consolidate and pass2

--- dev-scripts/P-K-partition.pl
+++ dev-scripts/P-K-partition.pl
@@ -5,7 +5,18 @@ use lib qw(../lib);
 use FLAT::DFA; use FLAT::NFA; use FLAT::PFA; use FLAT::Regex::WithExtraOps;
 use Data::Dumper;
 
-my $dfa = FLAT::Regex::WithExtraOps->new($ARGV[0])->as_pfa->as_nfa->as_dfa->as_min_dfa->trim_sinks;
+my $dfa = FLAT::Regex::WithExtraOps->new('a')->as_pfa->as_nfa->as_dfa->as_min_dfa->trim_sinks;
+my %testnodelist = (0 => { 
+                           1 => 'a',
+                           4 => 'd',
+                         },
+                    1 => { 2 => 'b', },
+                    2 => { 3 => 'c', },
+                    3 => { },
+                    4 => { 5 => 'e', },
+                    5 => { 6 => 'f', },
+                    6 => { },
+                   );
 
 # secondary DFT over forest, of which each k-group
 # is a root node; all nodes not in a k-group, accessible
@@ -22,7 +33,7 @@ sub consolidate {
 
 sub pk_partition {
   my $dfa         = shift;
-  my %nodelist    = $dfa->as_node_list(); 
+  my %nodelist    = %testnodelist; # $dfa->as_node_list(); 
   my $lastDFLabel = 0; 
   my $TAINTED     = 0;
   my %dflabel     = (); # tracks dflabel 
@@ -31,7 +42,7 @@ sub pk_partition {
   my %kgroups     = (); # strong components + nodes reachable via them
   my %crossedges  = (); # tracks cross edges to check 
   pass1($dfa->get_starting(),\%nodelist,$lastDFLabel,\%dflabel,\%backtracked,$TAINTED,\%taintpoints,\%kgroups,\%crossedges);
-  #...
+  print STDERR Dumper(\%kgroups);
   %nodelist = consolidate(\%nodelist,\%kgroups,\%crossedges);
   pass2()
 }
@@ -48,33 +59,45 @@ sub pass1 {
   my $taintpoints = shift; # tracks taint points
   my $kgroups     = shift; # strong components + nodes reachable via them
   my $crossedges  = shift; # tracks cross edges to check 
+  # getting here indicates the following of an edge to a previously unvisited node, $startNode
   $dflabel->{$startNode} = ++$lastDFLabel;
-  foreach my $adjacent (keys(%{$nodelist})) {
-    if (!exists($dflabel->{$adjacent})) {                      # follow tree edge
+  if (0 < $TAINTED) {
+    $kgroups->{$TAINTED}->{$startNode}++;                      # add startNode to current kgroup if in taint-mode
+  }
+  # attempt to continue traversal on $startNode's adjacent nodes (all via edge directed outward)
+  foreach my $adjacent (keys(%{$nodelist->{$startNode}})) {
+print STDERR "$adjacent\n";
+    if (!exists($dflabel->{$adjacent})) {                      # follow tree edge (recursively call pass1(...))
       pass1($adjacent,$nodelist,$lastDFLabel,$dflabel,$backtracked,$TAINTED,$taintpoints,$kgroups,$crossedges);
-    } else {
-      print "$adjacent\n";
+    } else {                                                   # adjacent is a previously visited node
       if ($dflabel->{$adjacent} < $dflabel->{$startNode}) {
-        if (!exists($backtracked->{$adjacent})) {              # back edge !!
+        if (!exists($backtracked->{$adjacent})) {              # adjacent is a back edge 
           if (0 >= $TAINTED) {
             $TAINTED = $TAINTED*(-1)+1;
+            print STDERR "$TAINTED\n";
           }
+          # the following is not dependent on the condition, 0>=$TAINTED
           $taintpoints->{$adjacent}++; # track points at which back edges are detected
-          $kgroups->{$TAINTED}->{$adjacent}++; 
-        } else {                                               # cross edge
+          $kgroups->{$TAINTED}->{$adjacent}++;                 # add adjacent to current kgroup
+        } else {                                               # adjacent is a cross edge
+          # the following is not dependent on the condition, 0>=$TAINTED
+          # additionally, nothing is done for nodes accessible via cross edge until pass2
           $crossedges->{$startNode}->{$adjacent}++;
         }
       }
     }
   }
   # on back track
-  $backtracked->{$startNode} = 1;
-  if (exists($taintpoints->{$startNode})) {
-    delete($taintpoints->{$startNode});
-  }
-  # check to see if there are no more taintpoints
-  if (!keys(%{$taintpoints})) {
-    $TAINTED = $TAINTED*(-1);
+  $backtracked->{$startNode}++;                                # track for cross edge detection
+  if (0 < $TAINTED) {                                          # only check if in taint-mode 
+    if (exists($taintpoints->{$startNode})) {                  # remove $startNode from $taintpoints if it's a member
+      delete($taintpoints->{$startNode});
+    }
+    # check to see if there are no more taintpoints
+    if (!keys(%{$taintpoints})) {                              # taint-mode is off only if $taintpoints is empty
+      $TAINTED = $TAINTED*(-1);
+      print STDERR "$TAINTED\n";
+    }
   }
 }
 
