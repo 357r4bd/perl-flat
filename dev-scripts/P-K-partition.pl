d36ca71767799ff7685801dbb3dbc8c94dc6ed23
B. Estrade <estrabd@gmail.com>
2008-11-04 17:42:51 +0000

    put test graph in place, now need to correct code to fix revealed incorrectness

--- dev-scripts/P-K-partition.pl
+++ dev-scripts/P-K-partition.pl
@@ -4,18 +4,41 @@ use strict; use warnings;
 use lib qw(../lib);
 use FLAT::DFA; use FLAT::NFA; use FLAT::PFA; use FLAT::Regex::WithExtraOps;
 use Data::Dumper;
+use Tie::Hash::Indexed; # required to test algorithm, since perl hashes are not ordered
 
 my $dfa = FLAT::Regex::WithExtraOps->new('a')->as_pfa->as_nfa->as_dfa->as_min_dfa->trim_sinks;
-my %testnodelist = (0 => { 
-                           1 => 'a',
-                           4 => 'd',
-                         },
+
+# creates ordered hash ref for nested, ordered hashes needed to test various scenarios
+sub tieH {
+  tie my %hash, 'Tie::Hash::Indexed', @_;
+  return \%hash;
+}
+
+tie my %testnodelist, 'Tie::Hash::Indexed';
+   %testnodelist = (0 => tieH( 1 => 'a',    # finite
+                               4 => 'd',    # finite
+                               7 => 'g',    # infinite
+                              12 => 'o'),   # infinte 
                     1 => { 2 => 'b', },
                     2 => { 3 => 'c', },
                     3 => { },
                     4 => { 5 => 'e', },
                     5 => { 6 => 'f', },
                     6 => { },
+                    7 => { 8 => 'h' },
+                    8 => { 9 => 'i' },
+                    9 => tieH( 9 => 'l',    # self-loop
+                              10 => 'j',
+                               2 => 'n'),   # supposed to be a cross edge
+                   10 => tieH( 8 => 'k',
+                              11 => 'm'),
+                   11 => { },
+                   12 => { 13 => 'p',},
+                   13 => { 14 => 'q',},
+                   14 => { 15 => 'r',},
+                   15 => tieH(15 => 's',
+                              13 => 'u',
+                               5 => 't'),   # supposed to be a cross edge 
                    );
 
 # secondary DFT over forest, of which each k-group
