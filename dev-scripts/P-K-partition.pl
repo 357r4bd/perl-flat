4511963e5f3f0ca31f37862564f75b78874f491a
B. Estrade <estrabd@gmail.com>
2008-11-22 18:16:26 +0000

    made lots of progress; ready to implement the 2nd pass, which should be cake.

--- dev-scripts/P-K-partition.pl
+++ dev-scripts/P-K-partition.pl
@@ -6,10 +6,13 @@ use lib qw(../lib);
 use FLAT::DFA; use FLAT::NFA; use FLAT::PFA; use FLAT::Regex::WithExtraOps;
 use Data::Dumper;
 use Tie::Hash::Indexed; # required to test algorithm, since perl hashes are not ordered
+use Data::Dumper;
 
 my $DEBUG = 0;
 
-my $dfa = FLAT::Regex::WithExtraOps->new('a')->as_pfa->as_nfa->as_dfa->as_min_dfa->trim_sinks;
+my $trouble_re = 'abc(d)*e'; # so simple, yet so troublesome
+
+my $dfa = FLAT::Regex::WithExtraOps->new($trouble_re)->as_pfa->as_nfa->as_dfa->as_min_dfa->trim_sinks;
 
 # creates ordered hash ref for nested, ordered hashes needed to test various scenarios
 sub tieH {
@@ -47,17 +50,9 @@ tie my %testnodelist, 'Tie::Hash::Indexed';
 # secondary DFT over forest, of which each k-group
 # is a root node; all nodes not in a k-group, accessible
 # from a k-group is also in K
-sub pass2 {
 
-}
-
-# builds a new directed graph taking all nodes in
-# each k-group to be their own node
-sub consolidate {
-
-}
-
-{ ### closure to moderate access and modification to $TAINT and $lastDFLabel
+{ 
+  ### closure to moderate access and modification to $TAINT and $lastDFLabel
   my $lastDFLabel = 0; 
   sub next_dflabel { return ++$lastDFLabel;}
   sub get_dflabel  { return $lastDFLabel;}
@@ -70,44 +65,90 @@ sub consolidate {
 sub pk_partition {
   my $dfa         = shift;
   my %nodelist    = %testnodelist; # $dfa->as_node_list(); 
+  #my %nodelist   =  $dfa->as_node_list(); 
   my %dflabel     = (); # tracks dflabel 
   my %backtracked = (); # tracks nodes from which we've backtracked for cross edge detection
   my %taintpoints = (); # tracks taint points
   my %kgroups     = (); # strong components + nodes reachable via them
+  my %kadj        = (); # nodes adjacent to members of each kgroup
   my %crossedges  = (); # tracks cross edges to check 
-  pass1($dfa->get_starting(),\%nodelist,\%dflabel,\%backtracked,\%taintpoints,\%kgroups,\%crossedges);
-  print STDERR Dumper(\%kgroups);
-  %nodelist = consolidate(\%nodelist,\%kgroups,\%crossedges);
-  pass2()
+
+  pass1($dfa->get_starting(),undef,\%nodelist,\%dflabel,\%backtracked,\%taintpoints,\%kgroups,\%kadj,\%crossedges);
+
+  trim_kgroup_internal_edges(\%kgroups,\%kadj); # removes all edges to fellow kgroup members, leaving only edges leaving the kgroups
+
+  # faciliate dfs over forest now, starting at the designated edges in %kadj
+  my $parent = undef;
+  my %kreachable  = (); # will track all non-k nodes reachable by kgroups
+  foreach my $k (keys(%kadj)) {
+    foreach my $startNode (keys(%{$kadj{$k}})) {
+      $parent = $kadj{$k}{$startNode};
+      pass2($startNode,$parent,\%nodelist,\%dflabel,\%kreachable);
+    }
+  }
+}
+
+sub trim_kgroup_internal_edges {
+  my $kgroups     = shift; # strong components + nodes reachable via them
+  my $kadj        = shift; # nodes adjacent to members of each kgroup
+  foreach my $k (keys(%{$kgroups})) {
+    foreach my $kmember (keys(%{$kgroups->{$k}})) {
+      delete($kadj->{$k}->{$kmember}); 
+    }
+  }
+}
+
+# second pass - starting with each k group; the 
+# idea is to find all nodes reachable by all k-groups
+#
+sub pass2 {
+  my $startNode   = shift;
+  my $parent      = shift;
+  my $nodelist    = shift;
+  my $dflabel     = shift; # tracks dflabel 
+  my $kreachable  = shift;
+
+# ... need to implement this searching
+
 }
 
 # initial DFT, detecting strong components *and*
 # the nodes which are accessible by them
 sub pass1 {
   my $startNode   = shift;
+  my $parent      = shift;
   my $nodelist    = shift;
   my $dflabel     = shift; # tracks dflabel 
   my $backtracked = shift; # tracks nodes from which we've backtracked for cross edge detection
   my $taintpoints = shift; # tracks taint points
   my $kgroups     = shift; # strong components + nodes reachable via them
+  my $kadj        = shift; # nodes adjacent to members of each kgroup
   my $crossedges  = shift; # tracks cross edges to check 
 
   print STDERR "start node: $startNode\n" if ($DEBUG);
 
   # getting here indicates the following of an edge to a previously unvisited node, $startNode
   $dflabel->{$startNode} = next_dflabel(); 
+
   if (0 < get_taint()) {
     $kgroups->{get_taint()}->{$startNode}++;                   # add startNode to current kgroup if in taint-mode
+    foreach my $ka (keys(%{$nodelist->{$startNode}})) {  # store all nodes adjacent to each node in kgroup
+      $kadj->{get_taint()}->{$ka} = $parent; 
+    }
   }
+
   # attempt to continue traversal on $startNode's adjacent nodes (all via edge directed outward)
   foreach my $adjacent (keys(%{$nodelist->{$startNode}})) {
     if (!exists($dflabel->{$adjacent})) {                      # follow tree edge (recursively call pass1(...))
-      pass1($adjacent,$nodelist,$dflabel,$backtracked,$taintpoints,$kgroups,$crossedges);
-      # on back track
+      pass1($adjacent,$startNode,$nodelist,$dflabel,$backtracked,$taintpoints,$kgroups,$kadj,$crossedges);
+                                                               # on back track
       $backtracked->{$adjacent}++;                             # track for cross edge detection
       print STDERR "back tracked from: $adjacent\n" if ($DEBUG);
       if (0 < get_taint()) {                                   # only check if in taint-mode 
         $kgroups->{get_taint()}->{$adjacent}++;                # add adjacent to current kgroup
+        foreach my $ka (keys(%{$nodelist->{$adjacent}})) {     # store all nodes adjacent to each node in kgroup
+          $kadj->{get_taint()}->{$ka} = $startNode; 
+        }
         if (exists($taintpoints->{$adjacent})) {               # remove $startNode from $taintpoints if it's a member
           delete($taintpoints->{$adjacent});
         }
@@ -130,9 +171,11 @@ sub pass1 {
           if (0 >= get_taint()) {
             toggle_taint_on();
           }
-          # the following is not dependent on the condition, 0>=$TAINTED
           $taintpoints->{$adjacent}++;                         # track points at which back edges are detected
           $kgroups->{get_taint()}->{$adjacent}++;              # add adjacent to current kgroup
+          foreach my $ka (keys(%{$nodelist->{$adjacent}})) {     # store all nodes adjacent to each node in kgroup
+            $kadj->{get_taint()}->{$ka} = $startNode; 
+          }
         } else {                                               # adjacent is a cross edge
           $crossedges->{$startNode}->{$adjacent}++;
           print STDERR "cross edge: $startNode -> $adjacent\n" if ($DEBUG);
